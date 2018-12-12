ad83ec9747f6ae9eada1b17d9cdde78fdb90018f
Marcelo Shen <CaogangShen@gmail.com>
2018-12-11 19:14:26 -0800

    Formatted timer appearance
    So basically, um, look, I make the texts look nicer, like this, and, um, like, I, like, well, make the slider timer text, well, like a normal timer text, yeah.

--- PracticeNRGScouting2018/MatchTimer.xaml.cs
+++ PracticeNRGScouting2018/MatchTimer.xaml.cs
@@ -35,5 +35,38 @@ namespace PracticeNRGScouting2018
         {
 
         }
+
+        private void TimeSlider_ValueChanged(object sender, ValueChangedEventArgs e)
+        {
+            timeValue.Text = numToTime(e.NewValue);
+        }
+        public static String numToTime(double a)
+        {
+            int num = (int) a;
+            int min = num / 60000;
+            int sec = (num % 60000) / 1000;
+            int millisec = num % 1000;
+            return (min + ":" + formatNum(sec, 2) + "." + formatNum(millisec, 3));
+
+        }
+
+        public static String formatNum(int num, int digits) // fill in 0 for better format
+        {
+            String result = "" + num;
+            int change = result.Length - digits;
+            if (change > 0)
+            {
+                result = result.Substring(0, digits);
+            }
+            else if (change < 0)
+            {
+                while (change < 0)
+                {
+                    result = "0" + result;
+                    change++;
+                }
+            }
+            return result;
+        }
     }
 }
