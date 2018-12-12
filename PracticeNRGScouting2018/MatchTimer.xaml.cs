8689fe9b5c5c1270ffc8923b9301e9ae91ad7bd6
Marcelo Shen <CaogangShen@gmail.com>
2018-12-12 14:35:52 -0800

    Made a timer
    for fun

--- PracticeNRGScouting2018/MatchTimer.xaml.cs
+++ PracticeNRGScouting2018/MatchTimer.xaml.cs
@@ -11,19 +11,55 @@ namespace PracticeNRGScouting2018
 {
 	public partial class MatchTimer : ContentPage
 	{
-		public MatchTimer ()
+        public static readonly int matchLengthMs = 150000;
+        public static readonly String cubePickedText = "Cube Picked";
+        public static readonly String cubeDroppedText = "Cube Dropped";
+        public static readonly String timerStart = "Start Timer";
+        public static readonly String timerPause = "Pause Timer";
+        private int timerValue = 0;
+
+        public MatchTimer ()
 		{
 			InitializeComponent ();
 		}
 
         private void ResetTimer_Clicked(object sender, EventArgs e)
         {
-
+            
         }
 
         private void TimeStart_Clicked(object sender, EventArgs e)
         {
+            if (timeStart.Text.Equals(timerStart)) //timerStart
+            {
+                Device.StartTimer(TimeSpan.FromMilliseconds(50), () =>
+                {
+                    if (timerValue >= matchLengthMs || timeStart.Text.Equals(timerPause))
+                    {
+                        timeStart.Text = timerPause;
+                        timerValue = matchLengthMs;
+                        timeValue.Text = numToTime(timerValue);
+                        return false;
+                    }
+                    timerElapsed();
+                    return true;
+                });
+            }
+            else if (timeStart.Text.Equals(timerPause))
+            {
+                timeStart.Text = timerStart;
+            }
+            else
+            {
 
+            }
+        }
+
+        private void timerElapsed()
+        {
+            timerValue += 50;
+            timeValue.Text = numToTime(timerValue);
+            timeSlider.Value = timerValue;
         }
 
         private void ClimbStart_Clicked(object sender, EventArgs e)
@@ -39,6 +75,7 @@ namespace PracticeNRGScouting2018
         private void TimeSlider_ValueChanged(object sender, ValueChangedEventArgs e)
         {
             timeValue.Text = numToTime(e.NewValue);
+            timerValue = (int) e.NewValue;
         }
         public static String numToTime(double a)
         {
@@ -46,7 +83,7 @@ namespace PracticeNRGScouting2018
             int min = num / 60000;
             int sec = (num % 60000) / 1000;
             int millisec = num % 1000;
-            return (min + ":" + formatNum(sec, 2) + "." + formatNum(millisec, 3));
+            return (min + ":" + sec.ToString("D2") + "." + formatNum(millisec,2)); //formatNum(sec, 2) //formatNum(millisec, 2)
 
         }
 
@@ -60,11 +97,16 @@ namespace PracticeNRGScouting2018
             }
             else if (change < 0)
             {
-                while (change < 0)
-                {
-                    result = "0" + result;
-                    change++;
-                }
+                result = repeatString("0", -change) + result;
+            }
+            return result;
+        }
+        public static String repeatString(String str, int times)
+        {
+            String result = "";
+            for (int i = 0; i < times; i++)
+            {
+                result += str;
             }
             return result;
         }
