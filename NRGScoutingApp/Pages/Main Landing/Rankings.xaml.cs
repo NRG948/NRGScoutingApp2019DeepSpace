f5be6df85921b86c8a25d8c04c1fc9d4ecf6274e
Shrey Aeron <shreystechtips@gmail.com>
2019-02-08 14:48:50 -0800

    Changed Rank type implementation

--- NRGScoutingApp/Pages/Main Landing/Rankings.xaml.cs
+++ NRGScoutingApp/Pages/Main Landing/Rankings.xaml.cs
@@ -13,56 +13,90 @@ namespace NRGScoutingApp
         public Rankings()
         {
             InitializeComponent();
+            rankPicker.SelectedIndex = 0;
         }
 
         public static string teamSend;
 
-        MatchFormat.CHOOSE_RANK_TYPE rankChoice = MatchFormat.CHOOSE_RANK_TYPE.overallRank;
+        MatchFormat.CHOOSE_RANK_TYPE rankChoice;
+
 
         //Initializes the ranking object
         Ranker mainRank = new Ranker(App.Current.Properties["matchEventsString"].ToString());
 
-        void overallRank(object sender, System.EventArgs e)
+        void rankTypeDelta(object sender, System.EventArgs e)
         {
-            rankChoice = MatchFormat.CHOOSE_RANK_TYPE.overallRank;
+            switch (rankPicker.SelectedIndex)
+            {
+                case 0:
+                    rankChoice = MatchFormat.CHOOSE_RANK_TYPE.overallRank;
+                    break;
+                case 1:
+                    rankChoice = MatchFormat.CHOOSE_RANK_TYPE.pick1; //Hatch
+                    break;
+                case 2:
+                    rankChoice = MatchFormat.CHOOSE_RANK_TYPE.pick2; //Cargo
+                    break;
+                case 3:
+                    rankChoice = MatchFormat.CHOOSE_RANK_TYPE.climb; //Climb
+                    break;
+                case 4:
+                    rankChoice = MatchFormat.CHOOSE_RANK_TYPE.drop1; //Lvl1
+                    break;
+                case 5:
+                    rankChoice = MatchFormat.CHOOSE_RANK_TYPE.drop2; //lvl2
+                    break;
+                case 6:
+                    rankChoice = MatchFormat.CHOOSE_RANK_TYPE.drop3; //lvl3
+                    break;
+                 default:
+                    rankChoice = MatchFormat.CHOOSE_RANK_TYPE.overallRank;
+                    break;
+            }
             updateEvents();
         }
 
-        void pick1Rank(object sender, System.EventArgs e)
-        {
-            rankChoice = MatchFormat.CHOOSE_RANK_TYPE.drop1;
-            updateEvents();
-        }
+        //void overallRank(object sender, System.EventArgs e)
+        //{
+        //    rankChoice = MatchFormat.CHOOSE_RANK_TYPE.overallRank;
+        //    updateEvents();
+        //}
 
-        void pick2Rank(object sender, System.EventArgs e)
-        {
-            rankChoice = MatchFormat.CHOOSE_RANK_TYPE.drop2;
-            updateEvents();
-        }
+        //void pick1Rank(object sender, System.EventArgs e)
+        //{
+        //    rankChoice = MatchFormat.CHOOSE_RANK_TYPE.drop1;
+        //    updateEvents();
+        //}
 
-        void climbRank(object sender, System.EventArgs e)
-        {
-            rankChoice = MatchFormat.CHOOSE_RANK_TYPE.climb;
-            updateEvents();
-        }
+        //void pick2Rank(object sender, System.EventArgs e)
+        //{
+        //    rankChoice = MatchFormat.CHOOSE_RANK_TYPE.drop2;
+        //    updateEvents();
+        //}
 
-        void drop1Rank(object sender, System.EventArgs e)
-        {
-            rankChoice = MatchFormat.CHOOSE_RANK_TYPE.drop1;
-            updateEvents();
-        }
+        //void climbRank(object sender, System.EventArgs e)
+        //{
+        //    rankChoice = MatchFormat.CHOOSE_RANK_TYPE.climb;
+        //    updateEvents();
+        //}
 
-        void drop2Rank(object sender, System.EventArgs e)
-        {
-            rankChoice = MatchFormat.CHOOSE_RANK_TYPE.drop2;
-            updateEvents();
-        }
+        //void drop1Rank(object sender, System.EventArgs e)
+        //{
+        //    rankChoice = MatchFormat.CHOOSE_RANK_TYPE.drop1;
+        //    updateEvents();
+        //}
 
-        void drop3Rank(object sender, System.EventArgs e)
-        {
-            rankChoice = MatchFormat.CHOOSE_RANK_TYPE.drop3;
-            updateEvents();
-        }
+        //void drop2Rank(object sender, System.EventArgs e)
+        //{
+        //    rankChoice = MatchFormat.CHOOSE_RANK_TYPE.drop2;
+        //    updateEvents();
+        //}
+
+        //void drop3Rank(object sender, System.EventArgs e)
+        //{
+        //    rankChoice = MatchFormat.CHOOSE_RANK_TYPE.drop3;
+        //    updateEvents();
+        //}
 
         protected override void OnAppearing()
         {
