7a51607430b3091d2b34622a2b7695e6d22650e5
Shrey Aeron <shreystechtips@gmail.com>
2019-03-06 17:21:48 -0800

    Fixed Github merge issue

--- NRGScoutingApp/Pages/Pit Scouting/PitEntry.xaml.cs
+++ NRGScoutingApp/Pages/Pit Scouting/PitEntry.xaml.cs
@@ -4,85 +4,62 @@ using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 using Xamarin.Essentials;
 using Xamarin.Forms;
+using System.Collections.Generic;
 
 namespace NRGScoutingApp {
     public partial class PitEntry : ContentPage {
-        //Object for storing all the pit notes for JSON conversion
-        public class PitQs {
-            public String team { get; set; }
-            public String q0 { get; set; }
-            public String q1 { get; set; }
-            public String q2 { get; set; }
-            public String q3 { get; set; }
-            public String q4 { get; set; }
-            public String q5 { get; set; }
-            public String q6 { get; set; }
-            public String q7 { get; set; }
-            public String q8 { get; set; }
-            public String q9 { get; set; }
-            public String q10 { get; set; }
-        }
-
-        PitQs vals = new PitQs {
-            q0 = "",
-            q1 = "",
-            q2 = "",
-            q3 = "",
-            q4 = "",
-            q5 = "",
-            q6 = "",
-            q7 = "",
-            q8 = "",
-            q9 = "",
-            q10 = "",
-            team = Preferences.Get ("teamName", "couldNotSetTeam")
-        };
-
+       //Object for storing all the pit notes for JSON conversion
+        public String[] vals = new string[ConstantVars.QUESTIONS.Length + 1];
+       
         protected override bool OnBackButtonPressed () {
             return true;
         }
-
+        private Editor[] inputs = new Editor[ConstantVars.QUESTIONS.Length];
+        private Label[] questions = new Label[ConstantVars.QUESTIONS.Length];
+        String teamName;
         //The boolean will hide the delete button if the entry is new
-        public PitEntry (bool newCreation) {
+        public PitEntry (bool newCreation, String teamName) {
+            this.teamName = teamName;
             NavigationPage.SetHasBackButton (this, false);
             InitializeComponent ();
+            vals[vals.Length - 1] = Preferences.Get("teamStart", "memes not recieve");
+            for (int i = 0; i < inputs.Length; i++)
+            {
+                questions[i] = new Label
+                {
+                    Text = ConstantVars.QUESTIONS[i],
+                    MinimumWidthRequest = 1000,
+                    MinimumHeightRequest = 300,
+                    Margin = new Thickness(10, 5, 0, 0),
+                    FontAttributes = FontAttributes.Bold,
+                };
+                inputs[i] = new Editor
+                {
+                    Placeholder = "Type Here",
+                    MinimumWidthRequest = 1000,
+                    MinimumHeightRequest = 300,
+                    Margin = new Thickness(10, 5, 0, 0),
+                    Keyboard = Keyboard.Text,
+                    AutoSize = EditorAutoSizeOption.TextChanges
+                };
+                inputs[i].TextChanged += new EventHandler<TextChangedEventArgs>(Comment_Box_Updated);
+                mainLayout.Children.Add(questions[i]);
+                mainLayout.Children.Add(inputs[i]);
+            }
             deleteButton.IsVisible = !newCreation;
             Preferences.Set ("appState", 2);
-            setLabels ();
             cacheCheck ();
         }
 
-        void setLabels () {
-            q1.Text = ConstantVars.QUESTIONS[0];
-            q2.Text = ConstantVars.QUESTIONS[1];
-            q3.Text = ConstantVars.QUESTIONS[2];
-            q4.Text = ConstantVars.QUESTIONS[3];
-            q5.Text = ConstantVars.QUESTIONS[4];
-            q6.Text = ConstantVars.QUESTIONS[5];
-            q7.Text = ConstantVars.QUESTIONS[6];
-            q8.Text = ConstantVars.QUESTIONS[7];
-            q9.Text = ConstantVars.QUESTIONS[8];
-            q10.Text = ConstantVars.QUESTIONS[9];
-            q11.Text = ConstantVars.QUESTIONS[10];
-        }
-
-        void Comment_Box_Updated (object sender, Xamarin.Forms.TextChangedEventArgs e) {
+        protected void Comment_Box_Updated (object sender, Xamarin.Forms.TextChangedEventArgs e) {
             updateAllBoxes ();
             updateItems ();
         }
 
         void updateAllBoxes () {
-            vals.q0 = q1Text.Text;
-            vals.q1 = q2Text.Text;
-            vals.q2 = q3Text.Text;
-            vals.q3 = q4Text.Text;
-            vals.q4 = q5Text.Text;
-            vals.q5 = q6Text.Text;
-            vals.q6 = q7Text.Text;
-            vals.q7 = q8Text.Text;
-            vals.q8 = q9Text.Text;
-            vals.q9 = q10Text.Text;
-            vals.q10 = q11Text.Text;
+            for(int i = 0; i < vals.Length-1; i++) {
+                vals[i] = inputs[i].Text;
+            }
         }
 
         async void deleteClicked (object sender, System.EventArgs e) {
@@ -100,7 +77,7 @@ namespace NRGScoutingApp {
                 }
                 Preferences.Set ("matchEventsString", JsonConvert.SerializeObject (data));
                 try {
-                    Navigation.PopAsync();
+                    await Navigation.PopAsync(true);
                 } catch (System.NullReferenceException) { }
                 clearMatchItems ();
             }
@@ -111,7 +88,7 @@ namespace NRGScoutingApp {
             if (text) {
                 clearMatchItems ();
                 try {
-                    Navigation.PopAsync();
+                    await Navigation.PopAsync();
                 } catch (System.NullReferenceException) {
 
                 }
@@ -119,7 +96,14 @@ namespace NRGScoutingApp {
         }
 
         void updateItems () {
-            Preferences.Set ("tempPitNotes", JsonConvert.SerializeObject (vals));
+            Dictionary<String, String> temp = new Dictionary<string, string>();
+            Console.WriteLine("DAM IT");
+            for (int i = 0; i < vals.Length-1; i++) {
+                temp.Add("q" + i, vals[i]);
+                Console.WriteLine(vals[i]);
+            }
+            temp["team"] =  vals[vals.Length-1];
+            Preferences.Set("tempPitNotes", JsonConvert.SerializeObject(temp));
         }
 
         //Clears all properties for use in next match
@@ -133,11 +117,21 @@ namespace NRGScoutingApp {
             //Disables save button so app doesn't crash when user taps many times
             saveButton.IsEnabled = false;
 
-            vals.team = Preferences.Get ("teamStart", "oof");
-            JObject notes = JObject.FromObject (vals);
+            vals[vals.Length-1] = teamName;
+            Console.WriteLine("team");
+            Console.WriteLine(vals[vals.Length - 1]);
+            Console.WriteLine(Preferences.Get("teamStart", "oof"));
+            Dictionary<String,String> s = new Dictionary<String,String>();
+            for(int i = 0; i < vals.Length-1; i++) {
+                s.Add("q" + i,vals[i]);
+            }
+            s.Add("team", vals[vals.Length - 1]);
+            Console.WriteLine(teamName);
+            JObject notes = JObject.FromObject(s);
+            Console.WriteLine(notes);
             if (isAllEmpty (notes)) {
                 try {
-                    Navigation.PopAsync();
+                    Navigation.PopAsync(true);
                 } catch (System.InvalidOperationException) { }
                 clearMatchItems ();
             } else {
@@ -164,10 +158,11 @@ namespace NRGScoutingApp {
         async void pushBackToHome (JObject data, JArray temp, JObject parameters) {
             temp.Add (new JObject (parameters));
             data["PitNotes"] = temp;
+            Console.WriteLine(temp);
             Preferences.Set ("matchEventsString", JsonConvert.SerializeObject (data));
             Console.WriteLine (Preferences.Get ("matchEventsString", ""));
             try {
-                Navigation.PopAsync();
+                await Navigation.PopAsync(true);
             } catch (System.NullReferenceException) { }
             clearMatchItems ();
         }
@@ -190,55 +185,66 @@ namespace NRGScoutingApp {
 
         //Populates and checks in case of app crash
         void cacheCheck () {
-            JObject mainObject = new JObject ();
-            if (!String.IsNullOrWhiteSpace (Preferences.Get ("matchEventsString", ""))) {
-                mainObject = JObject.Parse (Preferences.Get ("matchEventsString", ""));
-            }
-            JArray scoutArray = new JArray ();
-            if (mainObject.ContainsKey ("PitNotes")) {
-                scoutArray = (JArray) mainObject["PitNotes"];
-            } else {
-                scoutArray = new JArray ();
-            }
-            String team = Preferences.Get ("teamStart", "oof");
-            PitQs temp;
-            try {
-                temp = JsonConvert.DeserializeObject<PitQs> (Preferences.Get ("tempPitNotes", ""));
-                Console.WriteLine (Preferences.Get ("tempPitNotes", ""));
-            } catch (JsonException) {
-                temp = vals;
-            }
-            //Attempting to set all text boxes
-            try {
-                if (scoutArray.Count > 0 && scoutArray.ToList ().Exists (x => x["team"].ToString ().Equals (team))) {
-                    var final = scoutArray.ToList ().Find (x => x["team"].ToString ().Equals (team));
-                    q1Text.Text = final["q0"].ToString ();
-                    q2Text.Text = final["q1"].ToString ();
-                    q3Text.Text = final["q2"].ToString ();
-                    q4Text.Text = final["q3"].ToString ();
-                    q5Text.Text = final["q4"].ToString ();
-                    q6Text.Text = final["q5"].ToString ();
-                    q7Text.Text = final["q6"].ToString ();
-                    q8Text.Text = final["q7"].ToString ();
-                    q9Text.Text = final["q8"].ToString ();
-                    q10Text.Text = final["q9"].ToString ();
-                    q11Text.Text = final["q10"].ToString ();
+            String team = Preferences.Get("teamStart", "oof");
+            Dictionary<String, String> temp = new Dictionary<string, string>();
+            String tempNotes = Preferences.Get("tempPitNotes", "");
+            if (!String.IsNullOrWhiteSpace(tempNotes))
+            {
+                try
+                {
+                    temp = JsonConvert.DeserializeObject<Dictionary<String, String>>(tempNotes);
                 }
-            } catch (System.NullReferenceException) { }
-            if (!Object.Equals (temp, null)) {
-                q1Text.Text += temp.q0;
-                q2Text.Text += temp.q1;
-                q3Text.Text += temp.q2;
-                q4Text.Text += temp.q3;
-                q5Text.Text += temp.q4;
-                q6Text.Text += temp.q5;
-                q7Text.Text += temp.q6;
-                q8Text.Text += temp.q7;
-                q9Text.Text += temp.q8;
-                q10Text.Text += temp.q9;
-                q11Text.Text += temp.q10;
+                catch (JsonException e)
+                {
+                    Console.WriteLine(e.StackTrace);
+                    temp = new Dictionary<String, String>();
+                }
             }
-            updateAllBoxes ();
+
+            if (temp.Count == 0)
+            {
+                JObject mainObject = new JObject();
+                if (!String.IsNullOrWhiteSpace(Preferences.Get("matchEventsString", "")))
+                {
+                    mainObject = JObject.Parse(Preferences.Get("matchEventsString", ""));
+                }
+                JArray scoutArray = new JArray();
+                if (mainObject.ContainsKey("PitNotes"))
+                {
+                    scoutArray = (JArray)mainObject["PitNotes"];
+                }
+                else
+                {
+                    scoutArray = new JArray();
+                }
+                try
+                {
+                    if (scoutArray.Count > 0 && scoutArray.ToList().Exists(x => x["team"].ToString().Equals(team)))
+                    {
+                        var final = scoutArray.ToList().Find(x => x["team"].ToString().Equals(team));
+                        for (int i = 0; i < inputs.Length; i++)
+                        {
+                            inputs[i].Text = (final["q" + i].ToString());
+                        }
+                    }
+                }
+                catch (System.NullReferenceException) { }
+            }
+            else
+            {
+                try
+                {
+                    for (int i = 0; i < inputs.Length; i++)
+                    {
+                        inputs[i].Text = temp["q" + i];
+                    }
+                }
+                catch (NullReferenceException e)
+                {
+
+                }
+            }
+            updateAllBoxes();
             updateItems ();
         }
     }
