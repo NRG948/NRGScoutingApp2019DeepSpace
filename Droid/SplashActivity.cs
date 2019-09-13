43a1a9f2f202b9b209043ee9517cf2ac67ec8e29
Marcelo Shen <CaogangShen@gmail.com>
2019-09-12 21:37:01 -0700

    INTROOOOOOO
    Reeeeeee

---
+++ Droid/SplashActivity.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using NRGScoutingApp.Droid;
+
+namespace NRGScouting
+{
+    [Activity(Theme = "@style/MyTheme.Splash", MainLauncher = true, NoHistory = true)]
+    public class SplashActivity : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Create your application here
+        }
+        protected override void OnResume()
+        {
+            base.OnResume();
+            Task startupWork = new Task(() => { Startup(); });
+            startupWork.Start();
+        }
+
+        async void Startup()
+        {
+            StartActivity(new Intent(Application.Context, typeof(MainActivity)));
+
+        }
+        public override void OnBackPressed() { }
+
+    }
+}
